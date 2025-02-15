691241daa88d400858228fffa74fb657e2d2d261
MtYeYe <135514581+FriendlyXuKun@users.noreply.github.com>
2025-02-16 00:18:39 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,59 @@
 #include "./LCL.h"
 
+int currentArgumentIndex = 0;
+int mode = DEBUG_MODE;
+std::string inputFileName = "";
+std::string outputFileName = "";
+
 int main(int argc, char *argv[])
 {
+    // If no arguments are passed, print usage
     if (argc == 1)
     {
         printUsage();
-        return 1;
+        return ERR_CODE;
     }
-    inputFileName = argv[1];
+    inputFileName = parseNextArgument();
     outputFileName = getFileName(inputFileName) + ".cpp";
-    currentArgumentIndex = 2;
+    // Parse arguments
     while (currentArgumentIndex < argc)
     {
         std::string arg = parseNextArgument();
         if (arg == "-h" || arg == "--help")
         {
             printUsage();
-            return 0;
+            return END_CODE;
         }
         else if (arg == "-v" || arg == "--version")
         {
             displayVersion();
-            return 0;
+            return END_CODE;
         }
         else if (arg == "-o" || arg == "--output")
         {
             if (currentArgumentIndex < argc)
             {
                 outputFileName = parseNextArgument();
+                logMessage("The filename is " + outputFileName);
+                if (!isFile(outputFileName))
+                {
+                    printERR("The filename '" + outputFileName + "' is invalid.");
+                    printUsage();
+                    return ERR_CODE;
+                }
             }
             else
             {
-                printERR("missing output file name");
-                return 1;
+                printERR("Missing output file name.");
+                return ERR_CODE;
             }
         }
         else
         {
-            printERR("unrecognized command-line argument: '" + arg + "';");
+            printERR("Unrecognized command-line argument: '" + arg + "'.");
+            printUsage();
+            return ERR_CODE;
         }
     }
-    return 0;
+    return END_CODE;
 }
